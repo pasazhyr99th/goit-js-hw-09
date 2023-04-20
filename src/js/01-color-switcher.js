83d313f250a1d6b09898b2381b8edbdb2f6b8903
Mykhailo Popyzenko <mp.and.sp@ukr.net>
2023-04-20 19:59:43 +0300

    color-switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+let intervalId = null;
+
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.btnStop.disabled = true;
+
+refs.btnStart.addEventListener('click', onClickStart);
+refs.btnStop.addEventListener('click', onClickStop);
+
+function onClickStart() {
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+
+    intervalId = setInterval(() => {
+        refs.body.style.backgroundColor = `${getRandomHexColor()}`;
+    }, 1000);
+}
+
+function onClickStop() {
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+
+    clearInterval(intervalId);
+}
