6c81812701dc2a6980421ee720c1c0845b30ba79
Mykhailo Popyzenko <mp.and.sp@ukr.net>
2023-04-21 23:11:42 +0300

    promises done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  const delay = Number(e.target.elements.delay.value);
+  const step = Number(e.target.elements.step.value);
+  const amount = Number(e.target.elements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    const position = i;
+    const promDelay = delay + step * (i - 1);
+    createPromise(position, promDelay)
+      .then(({ position, promDelay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${promDelay}ms`);
+      })
+      .catch(({ position, promDelay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${promDelay}ms`);
+      });
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
